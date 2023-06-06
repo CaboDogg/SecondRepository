133ed1fe7c4d4e80b8827dae1488a96db37deb71
CaboDogg <ijustik813@gmail.com>
2023-06-06 16:24:59 +0300

    Проверил правильность создания объекта класса погоды из созданного сервиса

--- WeatherService/WeatherService.cpp
+++ WeatherService/WeatherService.cpp
@@ -1,9 +1,11 @@
 ﻿#include <iostream>
-
+#include "Weather.h"
+#include "JsonService.h"
 using namespace std;
 
 int main()
 {
-
+    JsonService js;
+    Weather w = js.getWeather("weather.json");
     return 0;
 }
